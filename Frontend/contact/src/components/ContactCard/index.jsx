de19107db0292304a8ebc0d88c601a1924af9391
Matheus Santos <eumtssantos@outlook.com>
2022-12-07 09:34:46 -0300

    fix: fixed some styled components

--- Frontend/contact/src/components/ContactCard/index.jsx
+++ Frontend/contact/src/components/ContactCard/index.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { ContactLi } from "./styles";
 
 function ContactCard({ contact }) {
--- Frontend/contact/src/components/ContactsList/index.jsx
+++ Frontend/contact/src/components/ContactsList/index.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import api from "../../services/api";
 import ContactCard from "../ContactCard";
 import EmptyCard from "../EmptyCard";
-import { ContactInfo, EmptyDiv, Ul } from "./styles";
+import { ContactInfo, Ul } from "./styles";
 
 function ContactsList() {
   const [myContactsList, setMyContactsList] = useState([]);
